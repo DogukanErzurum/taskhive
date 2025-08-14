6b0ae20d9dcc6d5e560cd1d3eb61ff0ec50c6fd9
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2025-08-14 15:16:49 +0300

    APIs have been written for the login and registration pages and the connections have been completed.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { cn } from "@/lib/utils";
+import { Toaster } from "sonner";
+import { QueryProvider } from "@/components/query-provider";
 
 import "./globals.css";
 
@@ -19,10 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={cn(inter. className, "antialiased min-h-screen" ) }
-      >
-        {children}
+      <body className={cn(inter.className, "antialiased min-h-screen")}>
+        <QueryProvider>
+          <Toaster />
+          {children}
+        </QueryProvider>
       </body>
     </html>
   );
