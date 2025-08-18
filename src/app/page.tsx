9e50aa9abcc0825fd47bae77763098c45f9f55d5
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2025-08-18 14:33:04 +0300

    login screen design and backend codes completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
   return (
-    <div className="flex gap-4">
-      <Button variant="primary">
-        Primary
+    <div>
+      Only visible to authorized users.
+      <Button onClick={() => mutate()}>
+        Logout
       </Button>
     </div>
-  )
-}
+  );
+};
